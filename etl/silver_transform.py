5a97643d48f0fa0ff03d38f74b3353e01c26a0a7
Nakulesh <nakuleshj1998@gmail.com>
2025-06-10 07:33:11 -0400

    Streamline script

--- etl/silver_transform.py
+++ etl/silver_transform.py
@@ -1,18 +1,9 @@
 import pandas as pd
-import psycopg2
 import datetime as dt
 from sqlalchemy import create_engine
 from numpy import nan
-import json, time
 
 def extract_from_bronze():
-    db_params={
-        'dbname': 'macro-datalake',
-        'user': 'test-user',
-        'password':'pass123',
-        'host': 'localhost',
-        'port': 5432
-    }
     engine=create_engine("postgresql+psycopg2://test-user:pass123@localhost/macro-datalake")
     with engine.connect() as conn:
             raw_data=pd.read_sql("SELECT * from bronze_fred_raw;",conn)
@@ -32,10 +23,11 @@ def transform(raw_data):
     row_cnt=0
     for i, row in raw_data.iterrows():
         obs=pd.DataFrame(row['response']['observations'])
+        
         obs.drop(['realtime_end','realtime_start'],axis=1,inplace=True)
           
         obs['series_id']=row['series_id']
-          
+
         obs['date']=pd.to_datetime(obs['date'])
 
         obs['frequency']=pd.infer_freq(obs['date'])
@@ -43,12 +35,10 @@ def transform(raw_data):
         obs.loc[obs['value']==".",'value']=nan          
         obs['value']=obs['value'].astype(float).round(2)
         obs['value']=obs['value'].interpolate()
-        
-        row_cnt+=obs.shape[0]
+
         transformed_data=pd.concat([transformed_data,obs],axis=0)
         
-    if transformed_data.shape[0] == row_cnt:
-         return transformed_data
+        return transformed_data
 
 if __name__=="__main__":
     raw_data=extract_from_bronze()
