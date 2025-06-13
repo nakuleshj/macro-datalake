49819fc521e1edb16bc40d2ee8a4cd0d187cdad7
Nakulesh <nakuleshj1998@gmail.com>
2025-06-13 05:55:02 -0400

    Remove definition for bronze table

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,11 +1,6 @@
-CREATE TABLE IF NOT EXISTS bronze_fred_raw (
-    series_id TEXT,
-    response JSONB,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 CREATE TABLE IF NOT EXISTS silver_fred_cleaned (
     series_id TEXT, 
     date DATE, 
     value FLOAT, 
     frequency TEXT
-)
+);
