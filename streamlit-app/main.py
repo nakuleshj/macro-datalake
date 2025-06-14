94bf602efe3488732a7e18d88679b1e0aa3dd0d7
Nakulesh <nakuleshj1998@gmail.com>
2025-06-14 18:38:12 -0400

    Add EDA tab

--- streamlit-app/main.py
+++ streamlit-app/main.py
@@ -6,6 +6,7 @@ import os
 
 DB_ENGINE=create_engine("postgresql+psycopg2://test-user:pass123@localhost/macro_datalake")
 
+st.set_page_config(layout="wide")
 
 @st.cache_data(ttl=600)
 def load_data():
@@ -17,21 +18,38 @@ def load_data():
 
 df = load_data()
 
-st.sidebar.title("MacroLake Dashboard")
-section = st.sidebar.radio("Navigate to", ["Trends", "View Data", "EDA"])
+st.title("MacroLake Exploratory Data Analysis (EDA)")
 
-if section == "Trends":
-    st.title("Indicator Time Series")
-    series = st.selectbox("Select indicator", df.columns)
-    show_30d_ma= st.checkbox("Add 30-day Moving Average")
-    show_90d_ma= st.checkbox("Add 90-day Moving Average")
-    fig = px.line(df, x=df.index, y=series, title=f"{series} Over Time")
-    st.plotly_chart(fig, use_container_width=True)
+st.subheader("Snapshot of Data:")
+st.dataframe(df.head(10),use_container_width=True)
 
-if section == "View Data":
-    st.title("Indicator Data")
+st.subheader("Data Summary")
+st.write(f"Total rows: {len(df)}")
+st.write(f"Unique indicators: {df.columns.nunique()}")
+st.write(f"Date range: {df.index.min().date()} → {df.index.max().date()}")
 
-    cols=st.multiselect("Select indicators:", df.columns,default=df.columns[0])
-    st_date=st.date_input("Start date:", value=df.index[0], min_value=df.index[0], max_value=df.index[-1], key=None, help=None, on_change=None, args=None, kwargs=None, format="YYYY/MM/DD", disabled=False, label_visibility="visible")
-    end_date=st.date_input("Start date:", value=df.index[-1], min_value=df.index[0], max_value=df.index[-1], key=None, help=None, on_change=None, args=None, kwargs=None, format="YYYY/MM/DD", disabled=False, label_visibility="visible")
-    st.dataframe(df[st_date:][cols],use_container_width=True)
+st.subheader("Trend")
+dist_series = st.selectbox("Select indicator for histogram", df.columns)
+dist_data = df[dist_series]
+fig_hist = px.line(dist_data, x=df.index, y=dist_data )
+st.plotly_chart(fig_hist, use_container_width=True)
+
+st.subheader("Value Distribution")
+
+fig_hist = px.histogram(dist_data, x=dist_data, nbins=30, title=f"{dist_series} Distribution")
+st.plotly_chart(fig_hist, use_container_width=True)
+
+
+st.subheader("Boxplot for Outlier Detection")
+fig_box = px.box(dist_data, y=dist_data, title=f"{dist_series} Boxplot")
+st.plotly_chart(fig_box, use_container_width=True)
+
+
+st.subheader("Correlation Heatmap")
+
+corr_cols=st.multiselect("Select Indicators:",options=df.columns,default=df.columns[0:2])
+
+corr=df[corr_cols].corr().round(2)
+fig=px.imshow(corr, text_auto=True, color_continuous_scale='rdylgn', title="Correlation Matrix")
+
+st.plotly_chart(fig)
